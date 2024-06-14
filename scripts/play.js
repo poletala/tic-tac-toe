1999898657291e089d637569ac92b8d09dcf0ae8
Alena <92329824+poletala@users.noreply.github.com>
2024-06-14 15:37:51 +0500

    LS comb

--- scripts/play.js
+++ scripts/play.js
@@ -20,40 +20,55 @@ function noughts() {
 
 
 
-// background image changing when user clicks on the cell
 let counter = 0
+let firstPlayer  = JSON.parse(localStorage.getItem('FirstPlayer')); // combs of FirstPlayer in the local storage
+let secondPlayer = JSON.parse(localStorage.getItem('SecondPlayer')); // combs of Second Player in the local storage
+
 let gameField = document.querySelector('.game-field')
 gameField.onclick = function (event) {
     counter++
-    console.log(counter)
+    // console.log(counter)
     let isCrosses = localStorage.getItem('crosses');
-    let isNoughts = localStorage.getItem('noughts');
+    let isNoughts = localStorage.getItem('noughts'); //find out which sign the user has chosen from the local storage
     let imgCellSecondPlayer = (isCrosses) ? 'url(./assets/O.png) center no-repeat, white' :
     (isNoughts) ? 'url(./assets/X.png) center no-repeat, white' : '';
     let imgCellFirstPlayer = (isCrosses) ? 'url(./assets/X.png) center no-repeat, white' :
-                (isNoughts) ? 'url(./assets/O.png) center no-repeat, white' : '';
+                (isNoughts) ? 'url(./assets/O.png) center no-repeat, white' : ''; //choose right cover of the cell
     if (counter % 2 === 0) { //even click
         event.target.style.background = imgCellSecondPlayer
-        event.target.style.backgroundSize = '50% 50%'
+        event.target.style.backgroundSize = '50% 50%' //background changing 
+        let cellId = event.target.id[4] //find out the id of the cell
+        console.log(' Second Player clicks on' + cellId)
+        if (!secondPlayer) { //if there are no data in the LS
+            secondPlayer = [cellId] //comb of the Second Player
+        } else { //if LS is not empty
+            secondPlayer.push(cellId)// new cell id adding
+        }
+        localStorage.setItem('SecondPlayer',  JSON.stringify(secondPlayer)); // the comb of the Second Player adding in LS
+        console.log('Second Player comb' + secondPlayer) 
     }
     else {
-        event.target.style.background = imgCellFirstPlayer
+        event.target.style.background = imgCellFirstPlayer //the same actions with the First Player
         event.target.style.backgroundSize = '50% 50%'
+        let cellId = event.target.id[4]
+        console.log('First Player clicks on' + cellId)
+        if (!firstPlayer) {
+            firstPlayer = [cellId]
+        } else {
+            firstPlayer.push(cellId)
+    }
+    localStorage.setItem('FirstPlayer', JSON.stringify(firstPlayer));
+    console.log('First Player comb' + firstPlayer)
     }
     if (counter === 10) { 
-        localStorage.setItem('crosses', (''))
-        localStorage.setItem('noughts', (''))
+        localStorage.setItem('crosses', ('')) //LS clearing
+        localStorage.setItem('noughts', ('')) //LS clearing
         alert('stop')
         return
     }
 
-                                                        //обновляется массив
-    let cellId = event.target.id[4]
-    let cellArr = []
-    cellArr.push(cellId)
-    console.log(cellArr)
-    if (cellArr[0]==='1') {  //сравнить массивы
-        console.log('win')
-    }
+  
     
 }
+
+let winCombs = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9],[7,5,3]]
