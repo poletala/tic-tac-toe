6f97286b7357b8bbf361629facf6000b609935ed
Alena <92329824+poletala@users.noreply.github.com>
2024-06-20 01:49:03 +0500

    timer,score,restart

--- scripts/play.js
+++ scripts/play.js
@@ -5,7 +5,12 @@ let firstPlayer  = JSON.parse(localStorage.getItem('FirstPlayer')); // combs of 
 let secondPlayer = JSON.parse(localStorage.getItem('SecondPlayer')); // combs of Second Player in the local storage (current game)
 let combosFirstPlayer = JSON.parse(localStorage.getItem('CombosFirstPlayer')); //
 let combosSecondPlayer = JSON.parse(localStorage.getItem('CombosSecondPlayer'));
+let FirstPlayerScore = JSON.parse(localStorage.getItem('First Player score')); 
+let SecondPlayerScore = JSON.parse(localStorage.getItem('Second Player score')); 
 let gameField = document.querySelector('.game-field')
+let gameScore = document.querySelector('#game-score')
+let playerTurnText = document.querySelector('.player-turn')
+let timer = document.querySelector('.timer')
 let winCombs = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9],[7,5,3]]
 let counter = 0
 let isCrosses
@@ -13,7 +18,41 @@ let isNoughts
 let isGameOver
 let isPlayAgain
 let isNotEmpty 
+let interval
+let minutes = 0
+let seconds = 0
 
+function timerGame() {
+    console.log(timer.textContent)
+    seconds++
+    if(seconds < 10) {
+        timer.textContent = '0:0' + seconds
+    }
+    if(seconds > 9) {
+        timer.textContent = '0:' + seconds
+    }
+     if(seconds > 59) {
+        minutes++
+        seconds = 0
+        timer.textContent = minutes + ':00' 
+
+    }
+    
+}
+function startTimer() {
+    interval = setInterval(timerGame, 1000)
+}
+
+function newGame() {
+    gameScore.textContent = '0 : 0'
+    clearGameScore()
+}
+
+function scoreOnload() { 
+    gameScore.textContent = (FirstPlayerScore && SecondPlayerScore) ? (`${FirstPlayerScore} : ${SecondPlayerScore}`) :
+        (FirstPlayerScore && !SecondPlayerScore) ? (`${FirstPlayerScore} : 0`) :
+        (!FirstPlayerScore && SecondPlayerScore) ? (`0 : ${SecondPlayerScore}`) : '0 : 0';
+}
 
 //save the inf abt user's choice using local storage
 function crosses() {
@@ -22,6 +61,7 @@ function crosses() {
     console.log('X' + isCrosses)
     localStorage.setItem('crosses', isCrosses);
     location.replace("./game.html")
+    
 }
 function noughts() {
     isCrosses = false
@@ -29,6 +69,7 @@ function noughts() {
     console.log('0' + isNoughts)
     localStorage.setItem('noughts', isNoughts);
     location.replace("./game.html")
+  
 }
 
 //click on array
@@ -48,6 +89,12 @@ function clearFirstAndSecondPlayer() {
     localStorage.removeItem('FirstPlayer');
     localStorage.removeItem('SecondPlayer');
 }
+
+function clearGameScore() {
+    localStorage.removeItem('First Player score');
+    localStorage.removeItem('Second Player score');
+}
+
 // comparing win combinations with players' combinations, determining the winner
 function winner() {
     let player = (counter % 2 === 0) ? secondPlayer : firstPlayer;
@@ -58,13 +105,24 @@ function winner() {
             console.log(`intersection of ${player} ` + intersection)
             alert(`And the winner is ${winnerText}!`)
             isGameOver = true
+            localStorage.setItem(`${winnerText} score`, (Number(localStorage.getItem(`${winnerText} score`))) + 1); // game score in LS
         }
-}
-}
-function emptyCheck(array) { //нажимать на занятую ячейку 
-    cellId
-    let isNotEmpty = array.includes(cellId)
-    console.log('is not empty' + isNotEmpty); 
+    }
+}   
+// 
+function combosOfPlayers() {
+    if (!combosFirstPlayer) {
+        combosFirstPlayer = [firstPlayer]
+    } else {
+       combosFirstPlayer.push(firstPlayer)
+    }
+    if (!combosSecondPlayer) {
+        combosSecondPlayer = [secondPlayer]
+    } else {
+        combosSecondPlayer.push(secondPlayer)
+    }
+    localStorage.setItem('CombosFirstPlayer', JSON.stringify(combosFirstPlayer));
+    localStorage.setItem('CombosSecondPlayer', JSON.stringify(combosSecondPlayer));
 }
 // game
 gameField.onclick = function (event) {
@@ -82,12 +140,8 @@ gameField.onclick = function (event) {
     let cellId = Number(event.target.id[4]) //find out the id of the cell
 
     if (counter % 2 === 0) { //even click
+        playerTurnText.textContent = 'Player 1’s Turn'
         
-
-
-        
-        
-
         if (!secondPlayer) { //if there are no data in LS
             secondPlayer = [cellId] //comb of the Second Player (current game)
             localStorage.setItem('SecondPlayer', JSON.stringify(secondPlayer));
@@ -108,19 +162,15 @@ gameField.onclick = function (event) {
        
     }
     else {
-        
-
-        
-       
+        playerTurnText.textContent = 'Player 2’s Turn'
         if (!firstPlayer) {
             firstPlayer = [cellId]
             localStorage.setItem('FirstPlayer', JSON.stringify(firstPlayer));
         } else {
-            if ([...secondPlayer, ...firstPlayer].includes(cellId)) {    //
+            if ([...secondPlayer, ...firstPlayer].includes(cellId)) {    
                 alert('Choose another one cell.')
                 return
             }
-
 
             firstPlayer.push(cellId)                                     
             let comboFirstPlayer = Array.from(firstPlayer)
@@ -135,21 +185,22 @@ gameField.onclick = function (event) {
 
     winner()
 
-    if ((firstPlayer.length + secondPlayer.length) === 9 && !isGameOver) { //на неверные нажатия тоже срабатывает
+    if ((firstPlayer.length + secondPlayer.length) === 9 && !isGameOver) { 
         alert('Nobody has won')
         isGameOver = true
     }
     if(isGameOver) {
+        combosOfPlayers()
         isPlayAgain = confirm('Play again?')
-
-        combosFirstPlayer = (!combosFirstPlayer) ? [firstPlayer] :  combosFirstPlayer.push(firstPlayer);
-        localStorage.setItem('CombosFirstPlayer', JSON.stringify(combosFirstPlayer));
-
-        combosSecondPlayer = (!combosSecondPlayer) ? [secondPlayer] : combosSecondPlayer.push(secondPlayer);
-        localStorage.setItem('CombosSecondPlayer', JSON.stringify(combosSecondPlayer));
-
     }
     if(isPlayAgain) {                       //прописать, если не согласны играть заново
+        location.replace("./index.html") 
+        
+        //
+        clearXO()
+        clearFirstAndSecondPlayer()
+    }
+    if(!isPlayAgain && isGameOver) {
         location.replace("./index.html") 
         clearXO()
         clearFirstAndSecondPlayer()
