1d9b84bd64eef1a575b32a89154f6f4bef1ed955
Alena <92329824+poletala@users.noreply.github.com>
2024-06-27 02:19:52 +0500

    fin

--- scripts/play.js
+++ scripts/play.js
@@ -1,11 +1,10 @@
-
 let crossesButton = document.querySelector('.crosses')
 let noughtsButton = document.querySelector('.noughts')
 let returnBack = document.querySelector('.arrow-back') 
 let firstPlayer  = JSON.parse(localStorage.getItem('FirstPlayer')); // combs of FirstPlayer in the local storage (current game)
 let secondPlayer = JSON.parse(localStorage.getItem('SecondPlayer')); // combs of Second Player in the local storage (current game)
 let combosFirstPlayer = JSON.parse(localStorage.getItem('CombosFirstPlayer')); //
-let combosSecondPlayer = JSON.parse(localStorage.getItem('CombosSecondPlayer'));
+let combosSecondPlayer = JSON.parse(localStorage.getItem('CombosSecondPlayer'));//
 let FirstPlayerScore = JSON.parse(localStorage.getItem('First Player score')); 
 let SecondPlayerScore = JSON.parse(localStorage.getItem('Second Player score')); 
 let gameField = document.querySelector('.game-field')
@@ -18,48 +17,53 @@ let isCrosses
 let isNoughts
 let isGameOver
 let isPlayAgain
-let isNotEmpty 
 let interval 
 let minutes = 0
 let seconds = 0
-function onePlayerGame() {
+function pageOnload() {
+    if (localStorage.getItem('isBot') === null) {
+        document.querySelector('.game-score').classList.add('invisible-element')
+        document.querySelector('.new-game').classList.add('invisible-element')
+        document.querySelector('.choose-button').classList.add('invisible-element')
+    }
+    if (localStorage.getItem('isBot')) {
+        document.querySelector('.players-quantity').classList.add('invisible-element')
+    }
+    if (localStorage.getItem('isBot') === 'true') {
+        document.querySelector('.quantity-text').innerHTML = 'One Player Game'
+    }
+    if (localStorage.getItem('isBot') === 'false') {
+        document.querySelector('.quantity-text').innerHTML = 'Two Players Game'
+    }
+}
+function onePlayerGame() { 
     document.querySelector('.quantity-text').innerHTML = 'One Player Game'
     document.querySelector('.players-quantity').classList.add('invisible-element')
+    setTimeout(() => {
+        document.querySelector('.choose-button').classList.remove('invisible-element')
+    }, 500);
     localStorage.setItem('isBot', true)
 }
 function twoPlayersGame() {
     document.querySelector('.quantity-text').innerHTML = 'Two Players Game'
     document.querySelector('.players-quantity').classList.add('invisible-element')
+    setTimeout(() => {
+        document.querySelector('.choose-button').classList.remove('invisible-element')
+    }, 500);
     localStorage.setItem('isBot', false)
 }
-function timerGame() {
-    seconds++
-    if(seconds < 10) {
-        timer.textContent = minutes + ':0' + seconds
-    }
-    if(seconds > 9) {
-        timer.textContent = minutes + ':' + seconds
-    }
-     if(seconds > 59) {
-        minutes++
-        seconds = 0
-        timer.textContent = minutes + ':0' + seconds
-    }
-    
-}
-function startTimer() {
-    interval = setInterval(timerGame, 1000)
-}
-
-function newGame() {
+function newGame() {//click on New Game btn
     gameScore.textContent = '0 : 0'
-    clearGameScore()
-    localStorage.removeItem('isBot') 
+    document.querySelector('.new-game').classList.add('invisible-element')
+    document.querySelector('.choose-button').classList.add('invisible-element')
     document.querySelector('.players-quantity').classList.remove('invisible-element')
     document.querySelector('.quantity-text').innerHTML = ''
     clearLS()
+    clearGameScore()
+    localStorage.removeItem('CombosFirstPlayer')
+    localStorage.removeItem('CombosSecondPlayer')
+    localStorage.removeItem('isBot')
 }
-
 function scoreOnload() { 
     gameScore.textContent = (FirstPlayerScore && SecondPlayerScore) ? (`${FirstPlayerScore} : ${SecondPlayerScore}`) :
         (FirstPlayerScore && !SecondPlayerScore) ? (`${FirstPlayerScore} : 0`) :
@@ -81,26 +85,37 @@ function noughts() {
     console.log('0' + isNoughts)
     localStorage.setItem('noughts', isNoughts);
     location.replace("./game.html")
-  
 }
-
-//click on array
+function timerGame() {
+    seconds++
+    if(seconds < 10) {
+        timer.textContent = minutes + ':0' + seconds
+    }
+    if(seconds > 9) {
+        timer.textContent = minutes + ':' + seconds
+    }
+     if(seconds > 59) {
+        minutes++
+        seconds = 0
+        timer.textContent = minutes + ':0' + seconds
+    }
+}
+function startTimer() {
+    interval = setInterval(timerGame, 1000)
+}
+//click on back-btn
 returnBack.onclick = function() {
     location.replace("./index.html") 
     clearLS()
-    clearXO()
 }
 
-// delete 'crosses' and 'noughtes' keys from LS
-function clearXO() {
-    localStorage.removeItem('crosses') 
-    localStorage.removeItem('noughts') 
-}
-// delete 'FirstPlayer' and 'SecondPlayer' keys from LS
+// delete keys from LS
 function clearLS() {
     localStorage.removeItem('FirstPlayer');
     localStorage.removeItem('SecondPlayer');
     localStorage.removeItem('bot cell');
+    localStorage.removeItem('crosses') 
+    localStorage.removeItem('noughts') 
 }
 
 function clearGameScore() {
@@ -122,9 +137,7 @@ function winner() {
                 alert(`And the winner is ${winnerPlayerText}!`)
                 isGameOver = true
                 localStorage.setItem(`${winnerPlayerText} score`, (Number(localStorage.getItem(`${ winnerPlayerText} score`))) + 1); // game score in LS
-        
-                }}    }
-    
+                }}}
      else {
         let player = (counter % 2 === 0) ? secondPlayer : firstPlayer;
         let winnerText =  (counter % 2 === 0) ? 'Second Player' : 'First Player';
@@ -139,45 +152,8 @@ function winner() {
     }
 }
 }   
-// the cell choice of bot
-function botGame() {
-    let cellsPool = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    let excludePool = (firstPlayer && !secondPlayer) ? [firstPlayer] : [...firstPlayer, ...secondPlayer];
-    console.log('Исключаемые позиции для бота ' +  excludePool)
- 
-    let filteredCellsPool = []
-    for (let i=0;i<cellsPool.length;i++) {
-        if (excludePool.indexOf(cellsPool[i]) === -1) {
-            filteredCellsPool.push(cellsPool[i])
-        }
-    }
-    console.log('Возможные позиции для бота ' + filteredCellsPool)
-    let randomCellBot = filteredCellsPool[Math.floor(Math.random() * filteredCellsPool.length)]
-    console.log('Позиция бота ' + randomCellBot)
-    // localStorage.setItem('bot cell', randomCellBot)
-   
-    if (!secondPlayer) { //if there are no data in LS
-        secondPlayer = [randomCellBot] //comb of the Second Player (current game)
-        localStorage.setItem('SecondPlayer', JSON.stringify(secondPlayer));
-    } else { //if LS is not empty
-        secondPlayer.push(randomCellBot)
-        let comboSecondPlayer = Array.from(secondPlayer)// new cell id adding
-        console.log('Second Player comb ' + comboSecondPlayer) 
-        localStorage.setItem('SecondPlayer', JSON.stringify(comboSecondPlayer));
-    }
-    setTimeout(() => {
-        let imgCellSecondPlayer = (isCrosses) ? 'url(./assets/O.png) center no-repeat, white' :
-    (isNoughts) ? 'url(./assets/X.png) center no-repeat, white' : '';
-        document.querySelector(`#cell${randomCellBot}`).style.background = imgCellSecondPlayer
-        document.querySelector(`#cell${randomCellBot}`).style.backgroundSize = '50% 50%' //background changing 
-        console.log('Second Player clicks on ' + randomCellBot)
-    }, 500);
-}
 
-
-
-
-// 
+// ??
 function combosOfPlayers() {
     if (!combosFirstPlayer) {
         combosFirstPlayer = [firstPlayer]
@@ -196,10 +172,11 @@ function combosOfPlayers() {
 gameField.onclick = function (event) {
     isGameOver = false
     counter++
+
     let isCrosses = localStorage.getItem('crosses');
     let isNoughts = localStorage.getItem('noughts'); //find out which sign the user has chosen from the local storage
     
-    let isBot = localStorage.getItem('isBot')
+    let isBot = localStorage.getItem('isBot') // one-player mode or not
     
     let imgCellSecondPlayer = (isCrosses) ? 'url(./assets/O.png) center no-repeat, white' :
     (isNoughts) ? 'url(./assets/X.png) center no-repeat, white' : '';
@@ -208,9 +185,9 @@ gameField.onclick = function (event) {
     (isNoughts) ? 'url(./assets/O.png) center no-repeat, white' : ''; //choose right cover of the cell
     
     let cellId = Number(event.target.id[4]) //find out the id of the cell
-    if (isBot === 'false') {
 
-    
+    if (isBot === 'false') { // two-players mode
+
         if (counter % 2 === 0) { //even click
         playerTurnText.textContent = 'Player 1’s Turn'
         
@@ -222,7 +199,6 @@ gameField.onclick = function (event) {
                 alert('Choose another one cell.')
                 return
             }
-            
             secondPlayer.push(cellId)
             let comboSecondPlayer = Array.from(secondPlayer)// new cell id adding
             console.log('Second Player comb ' + comboSecondPlayer) 
@@ -234,7 +210,7 @@ gameField.onclick = function (event) {
        
         }
    
-        else {
+        else {//the same actions with the First Player
         playerTurnText.textContent = 'Player 2’s Turn'
         if (!firstPlayer) {
             firstPlayer = [cellId]
@@ -244,19 +220,19 @@ gameField.onclick = function (event) {
                 alert('Choose another one cell.')
                 return
             }
-
             firstPlayer.push(cellId)                                     
             let comboFirstPlayer = Array.from(firstPlayer)
             console.log('First Player comb ' + comboFirstPlayer)
             localStorage.setItem('FirstPlayer', JSON.stringify(comboFirstPlayer));
         }
-        event.target.style.background = imgCellFirstPlayer //the same actions with the First Player
-        event.target.style.backgroundSize = '50% 50%'
 
+        event.target.style.background = imgCellFirstPlayer 
+        event.target.style.backgroundSize = '50% 50%'
         console.log('First Player clicks on ' + cellId)
         }
     }
-    if (isBot==='true') {
+    if (isBot==='true') {//one-player mode
+        playerTurnText.textContent = ''
         if (!firstPlayer) {
             firstPlayer = [cellId]
             localStorage.setItem('FirstPlayer', JSON.stringify(firstPlayer));
@@ -276,10 +252,11 @@ gameField.onclick = function (event) {
 
         console.log('First Player clicks on ' + cellId)
        
+
+        //bot
         let cellsPool = [1, 2, 3, 4, 5, 6, 7, 8, 9]
         let excludePool = (firstPlayer && !secondPlayer) ? [firstPlayer] : [...firstPlayer, ...secondPlayer];
         console.log('Исключаемые позиции для бота ' +  excludePool)
-     
         let filteredCellsPool = []
         for (let i=0;i<cellsPool.length;i++) {
             if (excludePool.indexOf(cellsPool[i]) === -1) {
@@ -288,8 +265,8 @@ gameField.onclick = function (event) {
         }
         console.log('Возможные позиции для бота ' + filteredCellsPool)
         let randomCellBot = filteredCellsPool[Math.floor(Math.random() * filteredCellsPool.length)]
-        console.log('Позиция бота ' + randomCellBot)
-        // localStorage.setItem('bot cell', randomCellBot)
+        console.log('Позиция бота ' + randomCellBot) //bot cell choice
+
        
         if (!secondPlayer) { //if there are no data in LS
             secondPlayer = [randomCellBot] //comb of the Second Player (current game)
@@ -319,17 +296,17 @@ gameField.onclick = function (event) {
         combosOfPlayers()
         isPlayAgain = confirm('Play again?')
     }
-    if(isPlayAgain) {                       //прописать, если не согласны играть заново
-        location.replace("./index.html") 
+    if(isPlayAgain) {    
+        setTimeout(() => {
+            location.replace("./index.html") 
+        }, 500);                 
         
-        //
-        clearXO()
         clearLS()
     }
     if(!isPlayAgain && isGameOver) {
-        location.replace("./index.html") 
-        clearXO()
+        setTimeout(() => {
+            location.replace("./index.html") 
+        }, 500);   
         clearLS()
     }
-   
 }
